dee4e5a644ebf2a6e2ac8afaf521cc9e52b15720
aliasifk <sparkdevv@gmail.com>
2022-11-01 16:56:20 +0530

    Change version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if os.path.isfile("DESCRIPTION.rst"):
 
 setup(
     name='python-digitalocean-me',
-    version='1.18.0',
+    version='1.19.0',
     description='digitalocean.com API to manage Droplets and Images',
     author='Lorenzo Setale ( http://who.is.lorenzo.setale.me/? )',
     author_email='lorenzo@setale.me',
